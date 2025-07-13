9705a8c16c3ca69e7001d849d1f674e7e17ee66c
hammy15 <ohamm15@gmail.com>
2025-07-13 15:39:10 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_REAL_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
+  apiKey: "PASTE_YOUR_REAL_API_KEY_HERE",
+  authDomain: "sniffy-app.firebaseapp.com",
+  projectId: "sniffy-app",
+  storageBucket: "sniffy-app.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
