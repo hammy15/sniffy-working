92f1b76ee88224b1f003f76cf3bf58aed56659b5
hammy15 <ohamm15@gmail.com>
2025-07-13 15:08:14 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,3 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
+apiKey: "AIzaSyB...",
+authDomain: "sniffy-app.firebaseapp.com",
+projectId: "sniffy-app",
