5dc8d2eb2e4ec90d57dbac56ab084a3069751f44
hammy15 <ohamm15@gmail.com>
2025-07-13 15:36:41 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,11 @@
-// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN",
+  apiKey: "YOUR_REAL_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  storageBucket: "YOUR_PROJECT.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
