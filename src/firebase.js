ab539e86cc54971783d036936bd4b115f6b2f92e
hammy15 <ohamm15@gmail.com>
2025-07-13 20:48:11 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,12 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDl6Hitkmws_aPWBX4yF_uYrnN8CpHgS2k",
   authDomain: "sniffy-app-be555.firebaseapp.com",
   projectId: "sniffy-app-be555",
-  storageBucket: "sniffy-app-be555.firebasestorage.app",
+  storageBucket: "sniffy-app-be555.appspot.com",
   messagingSenderId: "219824031376",
   appId: "1:219824031376:web:428056bea4fdf638117a15"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
